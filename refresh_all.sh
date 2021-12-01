9285dec8118bf9f65016c8b85a8653609a864886
Ubuntu <ubuntu@ec-testbed-containers.sdsc.edu>
2021-12-01 06:06:49 +0000

    add notebook proxy

--- refresh_all.sh
+++ refresh_all.sh
@@ -1,4 +1,5 @@
 docker pull nsfearthcube/ec_facets_client:latest
 docker pull nsfearthcube/ec_facets_client:feat-binder
 docker pull nsfearthcube/ec_facets_api_nodejs:latest
+docker pull nsfearthcube/mknb:latest
 docker-compose --env-file env.beta  -f geocodes-dev-compose.yaml -f geocodes-compose.yaml -f docker-compose.yaml up -d
